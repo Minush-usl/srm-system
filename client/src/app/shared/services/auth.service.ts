a5789c3ff1073886a18fc52b5f2a816401bf3c68
Minush-usl <jn4ik2001@gmail.com>
2020-06-04 17:02:53 +0300

    login (client + server)

---
+++ client/src/app/shared/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user'
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  token = null
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  register() {
+
+  }
+
+  login(user: User): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>('/api/auth/login', user)
+      .pipe(
+        tap(
+          ({token}) => {
+            localStorage.setItem('auth-token', token),
+            this.setToken(token)
+          }
+        )
+      )
+  }
+
+  setToken(token: string) {
+    this.token = token
+  }
+
+  getToken(): string {
+    return this.token
+  }
+
+  idAuthenticated(): boolean {
+    return !!this.token
+  }
+
+  logout() {
+    this.setToken(null)
+    localStorage.clear()
+  }
+}
