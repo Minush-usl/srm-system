d64a10513a23ec9b5a2a88b3c8a51c878ccd40c5
Minush-usl <jn4ik2001@gmail.com>
2020-09-07 20:07:38 +0300

    fix keys try

--- config/keys.js
+++ config/keys.js
@@ -5,4 +5,9 @@
 //     module.exports = require('./keys.dev')
 // }
 
-module.exports = require('./keys.dev')
+// module.exports = require('./keys.dev.js')
+
+module.exports = {
+    mondoURL: 'mongodb+srv://kirill:Nwct6WtReLGn6I74@cluster0-2c9vq.mongodb.net/test?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
+}
